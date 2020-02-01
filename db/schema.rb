9ba80528719f59442e8301247375dabb10a7f5cf
github username <github email address>
2020-02-01 03:13:18 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_01_31_230750) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
